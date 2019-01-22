4a068bcf5393c7d4330b353513f22c1b3fabdcdd
Seobs <subi88@naver.com>
2019-01-22 15:29:31 +0900

    php-cs-fixer

--- app/Http/Controllers/Sucol/AlbumController.php
+++ app/Http/Controllers/Sucol/AlbumController.php
@@ -2,27 +2,27 @@
 
 namespace App\Http\Controllers\Sucol;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class AlbumController extends Controller
 {
-    public function index() {
-
+    public function index()
+    {
     }
 
     /**
      * (api) 전체 아티스트 앨범 정보 가져오기
      */
-    public function albumList() {
-
-        $data = array();
+    public function albumList()
+    {
+        $data = [];
 
         $albums = DB::select('select * from sucol_albums order by id desc');
 
         foreach ($albums as $album) {
-            $temp = array();
+            $temp = [];
 
             // album
             $temp['no'] = $album->id;
@@ -30,12 +30,12 @@ class AlbumController extends Controller
             $temp['cover_image'] = $album->image;
 
             // songs
-            $songs = array();
+            $songs = [];
 
-            $song_list = DB::select('select * from sucol_album_songs where album_id = ?', array($album->id));
+            $song_list = DB::select('select * from sucol_album_songs where album_id = ?', [$album->id]);
 
             foreach ($song_list as $song) {
-                $temp_song = array();
+                $temp_song = [];
                 $temp_song['id'] = $song->id;
                 $temp_song['artist'] = $song->artist;
                 $temp_song['title'] = $song->name;
@@ -53,13 +53,15 @@ class AlbumController extends Controller
         return response()->json($data);
     }
 
-    public function albumSongList($id) {
+    public function albumSongList($id)
+    {
     }
 
     /**
      * (api) 앨범 최초 등록
      */
-    public function addAlbum() {
+    public function addAlbum()
+    {
         $sql = "insert into sucol_albums (create_at) values (now())";
         $results = DB::insert($sql);
 
@@ -71,7 +73,8 @@ class AlbumController extends Controller
     /**
      * (api) 앨범 수정
      */
-    public function updateAlbum(Request $request) {
+    public function updateAlbum(Request $request)
+    {
         $id = $request->input('id');
         $title = $request->input('title');
         $image = $request->input('img');
@@ -81,26 +84,25 @@ class AlbumController extends Controller
         // album update
         try {
             $sql = "update sucol_albums set title = ?, image = ? where id = ?";
-            $result = DB::update($sql, array($title, $image, $id));
+            $result = DB::update($sql, [$title, $image, $id]);
 
             foreach ($songs as $song) {
                 if ($song['id'] == 'new') {
                     $sql = "insert into sucol_album_songs (album_id, artist, name, image, url, create_at) values (?, ?, ?, ?, ?, now())";
-                    $result = DB::insert($sql, array($id, $song['artist'], $song['title'], $song['image'], $song['url']));
+                    $result = DB::insert($sql, [$id, $song['artist'], $song['title'], $song['image'], $song['url']]);
                 } else {
                     $sql = "update sucol_album_songs set artist = ?, name = ?, image = ?, url = ? where id = ?";
-                    $result = DB::update($sql, array($song['artist'], $song['title'], $song['image'], $song['url'], $song['id']));
+                    $result = DB::update($sql, [$song['artist'], $song['title'], $song['image'], $song['url'], $song['id']]);
                 }
             }
 
             foreach ($deletesongs as $song_id) {
                 $sql = "delete from sucol_album_songs where id = ?";
-                $result = DB::delete($sql, array($song_id));
+                $result = DB::delete($sql, [$song_id]);
             }
 
             return $this->albumList();
         } catch (\Exception $e) {
-
             return response()->json($e);
         }
     }
@@ -108,11 +110,12 @@ class AlbumController extends Controller
     /**
      * (api) 앨범 삭제
      */
-    public function deleteAlbum(Request $request) {
+    public function deleteAlbum(Request $request)
+    {
         $id = $request->input('id');
 
         $sql = "delete from sucol_albums where id = ?";
-        $result = DB::delete($sql, array($id));
+        $result = DB::delete($sql, [$id]);
 
         if ($result) {
             return $this->albumList();
