ed2f775aa2eed621298fd55c102559d16ba52d43
Seobs <subi88@naver.com>
2019-03-11 09:59:52 +0900

    Modify Privacy Policy of dice

--- resources/views/dice/agreement.blade.php
+++ resources/views/dice/agreement.blade.php
@@ -1,14 +1,35 @@
-<p><b>주사위 굴리기겠지 개인정보 처리 방침</b></p>
-<span>주사위 굴리기겠지(이하&nbsp;"앱"&nbsp;이라함)&nbsp;는&nbsp;정보통신망&nbsp;이용촉진&nbsp;및&nbsp;정보보호&nbsp;등에&nbsp;관한&nbsp;법률,&nbsp;개인정보보호법,&nbsp;통신비밀보호법,&nbsp;전기통신사업법,&nbsp;등&nbsp;정보통신서비스제공자가&nbsp;준수하여야&nbsp;할&nbsp;관련&nbsp;법령상의&nbsp;개인정보보호&nbsp;규정을&nbsp;준수하며,&nbsp;관련&nbsp;법령에&nbsp;의거한&nbsp;개인정보취급방침을&nbsp;정하여&nbsp;이용자&nbsp;권익&nbsp;보호에&nbsp;최선을&nbsp;다하고&nbsp;있습니다.&nbsp;<span>&nbsp;</span></span>
-<br />
-<br />
-<p>■ 수집하는 개인정보의 항목 및 수집방법</p>
-<p>앱은 개인정보를 수집, 이용하지 않습니다. 다만 아래 광고플랫폼을 통해 광고를 제공하고 있습니다.</p>
-<p>- Admob : https://admob.google.com</p>
-<br />
-<p>■ 개인정보 관리 책임자 및 연락처</p>
-<p>- idiotlabs@gmail.com</p>
-<br />
-<p>■ 개인정보 처리방침 변경</p>
-<p>이 개인정보처리방침은 시행일로부터 적용되며, 법령 및 방침에 따른 변경내용의 추가, 삭제 및 정정이 있는 경우에는 변경사항의 시행 7일 전부터 공지사항을 통하여 고지할 것입니다.<p>
-<p>이 개인정보처리방침은 2018년 3월 10일부터 적용 됩니다.</p>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width'>
+    <title>Privacy Policy</title>
+    <style> body { font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; padding:1em; } </style>
+</head>
+<body>
+<h2>개인 정보 정책</h2>
+<p>이 페이지는 누군가 내 서비스를 사용하기로 결정한 경우 개인 정보의 수집, 사용 및 공개에 관한 정책을 방문자에게 알리는 데 사용됩니다.</p>
+<p>내 서비스 사용을 선택하면이 정책과 관련하여 정보를 수집하고 사용하는 것에 동의하게됩니다. 수집 한 개인 정보는 서비스를 제공하고 개선하는 데 사용됩니다. 본 개인 정보 취급 방침에 설명 된 경우를 제외하고 귀하의 정보를 다른 사람과 사용하거나 공유하지 않습니다.</p>
+<p><strong>정보 수집 및 사용</strong></p>
+<p>더 나은 경험을 위해 서비스를 사용하면서 개인 식별 정보를 제공 할 것을 요구할 수 있습니다. 귀하가 요청한 정보는 귀하의 장치에 보관되며 어떠한 방식으로도 귀하가 수집하지 않습니다.</p>
+<p>앱은 귀하를 식별하는 데 사용되는 정보를 수집 할 수있는 제 3 자 서비스를 사용합니다.</p>
+<div><p>앱에서 사용하는 타사 서비스 제공 업체의 개인 정보 취급 방침에 대한 링크</p>
+    <ul><li><a href="https://www.google.com/policies/privacy/" target="_blank">Google Play Services</a></li>
+        <li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank">AdMob</a></li>
+        <li><a href="https://firebase.google.com/policies/analytics" target="_blank">Firebase Analytics</a></li><!---->
+        <li><a href="https://fabric.io/privacy" target="_blank">Fabric</a></li><!----><!----><!---->
+    </ul>
+</div>
+<p><strong>로그 데이터</strong></p>
+<p>귀하가 서비스를 이용할 때마다 앱 오류가 발생하면 로그 데이터라고하는 휴대 전화에서 데이터 및 정보 (제 3 자 제품을 통해)를 수집합니다. 이 로그 데이터에는 기기의 인터넷 프로토콜 ( "IP") 주소, 기기 이름, 운영체제 버전, 내 서비스를 이용할 때의 앱 구성, 서비스 사용 시간 및 날짜 등의 정보가 포함될 수 있습니다.</p>
+<p><strong>쿠키</strong></p>
+<p>쿠키는 익명 고유 식별자로 일반적으로 사용되는 소량의 데이터가 포함 된 파일입니다. 이러한 정보는 사용자가 방문한 웹 사이트에서 브라우저로 전송되며 장치의 내부 메모리에 저장됩니다.</p>
+<p>이 서비스는 이러한 "쿠키"를 명시 적으로 사용하지 않습니다. 그러나 앱은 정보를 수집하고 서비스를 개선하기 위해 "쿠키"를 사용하는 타사 코드 및 라이브러리를 사용할 수 있습니다. 이러한 쿠키를 수락하거나 거절하거나 쿠키가 장치로 전송되는 시점을 알 수 있습니다. 쿠키를 거부하면이 서비스의 일부분을 사용하지 못할 수도 있습니다.</p>
+<strong>보안</strong></p>
+<p>저는 귀하의 개인 정보를 제공함에있어 귀하의 신뢰를 소중하게 생각합니다. 따라서 우리는 상업적으로 수용 가능한 수단을 사용하려고 노력하고 있습니다. 그러나 인터넷을 통한 전송 방법이나 전자 저장 방법은 100 % 안전하고 신뢰할 수 없으며 절대적인 보안을 보장 할 수는 없습니다.</p>
+<p><strong>연락처</strong></p>
+<p>개인 정보 보호 정책에 관한 질문이나 제안 사항이 있으면 언제든지 저에게 연락하십시오.</p>
+<p>Email : idiotlabs@gmail.com</p>
+</body>
+</html>
