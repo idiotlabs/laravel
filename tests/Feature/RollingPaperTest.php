de50e28de730deee5aab44cf0df899ff8a4702a1
Seobs <subi88@naver.com>
2020-05-28 16:46:27 +0900

    Add pest and change rolling_paper post test

--- tests/Feature/RollingPaperTest.php
+++ tests/Feature/RollingPaperTest.php
@@ -1,25 +1,17 @@
 <?php
 
-namespace Tests\Feature;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 
-class RollingPaperTest extends TestCase
-{
-    use RefreshDatabase;
+uses(RefreshDatabase::class);
 
-    public function testPost()
-    {
-        $data = [
-            'receiver' => str_repeat('a', 4),
-            'text' => str_repeat('a', 4),
-        ];
+test('post', function () {
+    $data = [
+        'receiver' => faker()->name,
+        'text' => faker()->text,
+    ];
 
-        $response = $this->post('/api/rollingpaper/wirte', $data);
+    $response = $this->post('/api/rollingpaper/wirte', $data);
 
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertDatabaseHas('rolling_paper', ['receiver' => $data['receiver'], 'note' => $data['text']]);
-    }
-}
+    assertEquals(200, $response->getStatusCode());
+    $this->assertDatabaseHas('rolling_paper', ['receiver' => $data['receiver'], 'note' => $data['text']]);
+});
