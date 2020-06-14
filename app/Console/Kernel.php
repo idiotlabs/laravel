0a5b686f27750bb765c1714f16d740146aa4fd4b
Seobs <subi88@naver.com>
2020-06-14 16:23:32 +0900

    Remove laravel telescopep

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('telescope:prune --hours=48')->daily();
 
         // WM Message spread
 //        $schedule->job(new WarmMessageSpread())->hourly()->between('8:00', '22:00');
